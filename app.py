a4f4379fa5e6ad07bc8b52aab69e60473720c367
NonnyJay <chinonso.aguonye@eng.uniben.edu>
2022-06-02 18:16:20 +0100

    Pre Migration

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 # Imports
 #----------------------------------------------------------------------------#
 
+import os
 import json
 import dateutil.parser
 import babel
@@ -514,12 +515,12 @@ if not app.debug:
 #----------------------------------------------------------------------------#
 
 # Default port:
-if __name__ == '__main__':
-    app.run()
+""" if __name__ == '__main__':
+    app.run() """
 
 # Or specify port manually:
-'''
+
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=port)
-'''
+
