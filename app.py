7854464710d1f454b049376e7b8c29d52ce77692
NonnyJay <chinonso.aguonye@eng.uniben.edu>
2022-06-03 05:42:48 +0100

    After first Migration

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from flask_sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
-
+from flask_migrate import Migrate
 from forms import *
 #----------------------------------------------------------------------------#
 # App Config.
@@ -27,7 +27,7 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 
 # TODO: connect to a local postgresql database
-
+migrate = Migrate(app, db)
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
@@ -515,12 +515,12 @@ if not app.debug:
 #----------------------------------------------------------------------------#
 
 # Default port:
-""" if __name__ == '__main__':
-    app.run() """
+if __name__ == '__main__':
+    app.run()
 
 # Or specify port manually:
 
-if __name__ == '__main__':
+""" if __name__ == '__main__':
     port = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port) """
 
